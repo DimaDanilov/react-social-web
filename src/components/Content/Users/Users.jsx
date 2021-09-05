1e79e28e25b505271daa4a6ee7a214b14c81dccf
DimaDanilov <dima.danilov.2012@mail.ru>
2021-09-05 15:32:57 +0300

    users axios-sideeffect fixed with new container component

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -1,96 +1,46 @@
-import React from 'react';
 import style from './Users.module.css'
-import avatar1 from '../../../img/icons/user_default.svg'
-import axios from 'axios'
+import defaultUserPhoto from '../../../img/icons/user_default.svg'
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersAmount(response.data.totalCount);
-        })
-    }
+const Users = (props) => {
+    let pages = props.pagesListing(props.usersAmount, props.pageSize, props.currentPage)
 
-    onPageChanged(pageNumber) {
-        /* Проверка на число (защита от кнопки "...") */
-        if (Number.isInteger(pageNumber)) {
-            this.props.setCurrentPage(pageNumber);
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-                this.props.setUsers(response.data.items);
-            })
-        }
-    }
-
-
-    // Функция отвечающая за поведение переключения страниц
-    pagesListing(usersAmount, pageSize, currentPage) {
-        // Рассчет итогового количества страниц
-        let pagesCount = Math.ceil(usersAmount / pageSize);
-
-        // Возвращение массива списка страниц
-        if (pagesCount >= 6) {
-            switch (currentPage) {
-                case 1:
-                case 2:
-                case 3:
-                    return [1, 2, 3, 4, 5, 6, "...", pagesCount];
-                case pagesCount:
-                case pagesCount - 1:
-                case pagesCount - 2:
-                    return [1, "...", pagesCount - 5, pagesCount - 4, pagesCount - 3, pagesCount - 2, pagesCount - 1, pagesCount];
-                default:
-                    return [1, "...", currentPage - 2, currentPage - 1, currentPage, currentPage + 1, currentPage + 2, "...", pagesCount];
-            }
-        } else if (0 < pagesCount && pagesCount < 6) {
-            let pageList = [];
-            for (let i = 1; i <= pagesCount; i++) {
-                pageList.push(i);
-            }
-            return pageList;
-        } else return [];
-    }
-
-    render() {
-        let pages = this.pagesListing(this.props.usersAmount, this.props.pageSize, this.props.currentPage)
-
-        return (
-            <div>
-                {/* Pagination - Список страниц пользователей */}
-                <div className={style.pages_list}>
-                    {pages.map(
-                        page =>
-                            <span
-                                className={[this.props.currentPage === page && style.selectedPage, style.pageIcon].join(' ')}
-                                onClick={() => this.onPageChanged(page)}>
-                                {page}
-                            </span>
-                    )}
-                </div>
-                {/* Карточки пользователей */}
-                <div className={style.users_container}>
-                    {
-                        this.props.usersData.map((user) =>
-                            <div key={user.id} className={style.user_card}>
-                                <div className={style.img_container}>
-                                    {/* Проверка наличия картинки в response. При отсутствии вывод заглушки */}
-                                    <img src={user.photos.small != null ? user.photos.small : avatar1} />
-                                    {/* Кнопка и её контент в зависимости от response */}
-                                    {user.followed
-                                        ? <button className={style.button} onClick={() => { this.props.unfollow(user.id) }}>Unfollow</button>
-                                        : <button className={style.button} onClick={() => { this.props.follow(user.id) }}>Follow</button>
-                                    }
-                                </div>
-                                <div className={style.user_info}>
-                                    <h2>{user.name}</h2>
-                                    <p>{user.status}</p>
-                                </div>
-                            </div>
-                        )
-                    }
-                </div>
+    return (
+        <div>
+            {/* Pagination - Список страниц пользователей */}
+            <div className={style.pages_list}>
+                {pages.map(
+                    page =>
+                        <span
+                            className={[props.currentPage === page && style.selectedPage, style.pageIcon].join(' ')}
+                            onClick={() => props.onPageChanged(page)}>
+                            {page}
+                        </span>
+                )}
             </div>
-        )
-    }
+            {/* Карточки пользователей */}
+            <div className={style.users_container}>
+                {
+                    props.usersData.map((user) =>
+                        <div key={user.id} className={style.user_card}>
+                            <div className={style.img_container}>
+                                {/* Проверка наличия картинки в response. При отсутствии вывод заглушки */}
+                                <img src={user.photos.small != null ? user.photos.small : defaultUserPhoto} />
+                                {/* Кнопка и её контент в зависимости от response */}
+                                {user.followed
+                                    ? <button className={style.button} onClick={() => { props.unfollow(user.id) }}>Unfollow</button>
+                                    : <button className={style.button} onClick={() => { props.follow(user.id) }}>Follow</button>
+                                }
+                            </div>
+                            <div className={style.user_info}>
+                                <h2>{user.name}</h2>
+                                <p>{user.status}</p>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Users
--- src/components/Content/Users/UsersContainer.jsx
+++ src/components/Content/Users/UsersContainer.jsx
@@ -1,6 +1,68 @@
-import { connect } from 'react-redux'
+import React from 'react';
+import axios from 'axios'
 import Users from './Users'
+import { connect } from 'react-redux'
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersAmountAC } from '../../../redux/users-reducer'
+
+class UsersContainerAPI extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersAmount(response.data.totalCount);
+        })
+    }
+
+    onPageChanged = (pageNumber) => {
+        /* Проверка на число (защита от кнопки "...") */
+        if (Number.isInteger(pageNumber)) {
+            this.props.setCurrentPage(pageNumber);
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+            })
+        }
+    }
+
+    // Функция отвечающая за поведение переключения страниц
+    pagesListing = (usersAmount, pageSize, currentPage) => {
+        // Рассчет итогового количества страниц
+        let pagesCount = Math.ceil(usersAmount / pageSize);
+
+        // Возвращение массива списка страниц
+        if (pagesCount >= 6) {
+            switch (currentPage) {
+                case 1:
+                case 2:
+                case 3:
+                    return [1, 2, 3, 4, 5, 6, "...", pagesCount];
+                case pagesCount:
+                case pagesCount - 1:
+                case pagesCount - 2:
+                    return [1, "...", pagesCount - 5, pagesCount - 4, pagesCount - 3, pagesCount - 2, pagesCount - 1, pagesCount];
+                default:
+                    return [1, "...", currentPage - 2, currentPage - 1, currentPage, currentPage + 1, currentPage + 2, "...", pagesCount];
+            }
+        } else if (0 < pagesCount && pagesCount < 6) {
+            let pageList = [];
+            for (let i = 1; i <= pagesCount; i++) {
+                pageList.push(i);
+            }
+            return pageList;
+        } else return [];
+    }
+
+    render() {
+        return <Users
+            onPageChanged={this.onPageChanged}
+            pagesListing={this.pagesListing}
+            usersAmount={this.props.usersAmount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            usersData={this.props.usersData}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+        />
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -17,10 +79,10 @@ const mapDispatchToProps = (dispatch) => {
         unfollow: (userId) => { dispatch(unfollowAC(userId)); },
         setUsers: (users) => { dispatch(setUsersAC(users)); },
         setCurrentPage: (currentPage) => { dispatch(setCurrentPageAC(currentPage)) },
-        setTotalUsersAmount: (usersAmount) => {dispatch(setTotalUsersAmountAC(usersAmount))}
+        setTotalUsersAmount: (usersAmount) => { dispatch(setTotalUsersAmountAC(usersAmount)) }
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersContainerAPI)
 
 export default UsersContainer
